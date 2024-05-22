3d2f0fe84e7f89510b95342eee4ff84a85d734a1
Gaurav Hegade <neph005@protonmail.com>
2024-05-22 18:53:29 +0530

    Add snippets to neovim configuration

--- lvim/config.lua
+++ lvim/config.lua
@@ -29,3 +29,13 @@ lvim.plugins = {
 }
 
 lvim.colorscheme = "tokyonight-night"
+lvim.builtin.nvimtree.setup.view.side = "right"
+
+-- custom keymaps
+vim.keymap.set({ "n", "x" }, "j", function()
+	return vim.v.count > 0 and "j" or "gj"
+end, { noremap = true, expr = true })
+
+vim.keymap.set({ "n", "x" }, "k", function()
+	return vim.v.count > 0 and "k" or "gk"
+end, { noremap = true, expr = true })
